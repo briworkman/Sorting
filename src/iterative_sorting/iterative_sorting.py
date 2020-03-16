c0ec3eef6f2e0e40e897252ed52d34fabecf84d4
Brianna Workman <brianna.workman@hotmail.com>
2020-03-16 16:26:10 -0500

    Takes out number of iterations in bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,16 +21,13 @@ def selection_sort(arr):
 def bubble_sort(arr):
     has_swapped = True
 
-    num_of_iterations = 0
-
     while(has_swapped):
         has_swapped = False
-        for i in range(len(arr) - num_of_iterations - 1):
+        for i in range(len(arr) - 1):
             if arr[i] > arr[i+1]:
                 # Swap
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 has_swapped = True
-        num_of_iterations += 1
     return arr
 
 
